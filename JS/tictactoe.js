7a942891c3ac8e2c9f6de9588de84e11ef2d2618
Casvdt <302284632@student.rocmondriaan.nl>
2023-11-20 10:26:54 +0100

    update

--- JS/tictactoe.js
+++ JS/tictactoe.js
@@ -1,4 +1,7 @@
-const startButton = document.querySelector('.start-button')
+const cells = document.querySelectorAll(".cell");
+const turnText = document.querySelector(".turnText");
+const restartButton = document.querySelector(".restartButton");
+const startButton = document.querySelector('.start-button');
 
 
 const winConditions = [
@@ -11,3 +14,40 @@ const winConditions = [
     [0, 4, 8],
     [2, 4, 6]
 ];
+
+let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let running = false;
+
+initializeGame();
+function initializeGame(){
+cells.forEach(cell => cell.addEventListener("click", cellClicked));
+restartButton.addEventListener("click", restartGame);
+turnText.textContent = `${currentPlayer}'s turn`;
+running = true;
+}
+
+function cellClicked(){
+const cellIndex = this.getAttribute(cellIndex);
+
+if (options [cellIndex] != "" || !running)
+return;
+}
+
+updateCell(this, cellIndex);
+checkWinner();
+
+
+function updateCell(cell,index){
+options[index] = currentPlayer;
+cell.textContent = currentPlayer;
+}
+function changePlayer(){
+
+}
+function checkWinner(){
+
+}
+function restartGame(){
+
+}

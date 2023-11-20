b90970db89cdc32e3226106dbf4fba0f74c0eb37
Casvdt <302284632@student.rocmondriaan.nl>
2023-11-20 14:17:32 +0100

    update

--- JS/tictactoe.js
+++ JS/tictactoe.js
@@ -1,7 +1,7 @@
 const cells = document.querySelectorAll(".cell");
 const turnText = document.querySelector(".turnText");
 const restartButton = document.querySelector(".restartButton");
-const startButton = document.querySelector('.start-button');
+
 
 
 const winConditions = [
@@ -20,6 +20,7 @@ let currentPlayer = "X";
 let running = false;
 
 initializeGame();
+
 function initializeGame() {
     cells.forEach(cell => cell.addEventListener("click", cellClicked));
     restartButton.addEventListener("click", restartGame);
@@ -36,18 +37,50 @@ function cellClicked() {
 
     updateCell(this, cellIndex);
     checkWinner();
-
 }
+
 function updateCell(cell, index) {
     options[index] = currentPlayer;
     cell.textContent = currentPlayer;
 }
 function changePlayer() {
-
+    currentPlayer = (currentPlayer == "X") ? "O" : "X";
+    turnText.textContent = `${currentPlayer}'s turn`
 }
 function checkWinner() {
+    let roundWon = false;
 
+    for (let i = 0; i < winConditions.length; i++) {
+        const condition = winConditions[i];
+        const cellA = options[condition[0]];
+        const cellB = options[condition[1]];
+        const cellC = options[condition[2]];
+
+        if (cellA == "" || cellB == "" || cellC == "") {
+            continue;
+        }
+        if (cellA == cellB && cellB == cellC) {
+            roundWon = true;
+            break;
+        }
+    }
+    if (roundWon) {
+        turnText.textContent = ` ${currentPlayer} wins!`;
+        running = false;
+    }
+    else if(!options.includes("")){
+        turnText.textContent = `Draw!`;
+        running = false;
+    } 
+    else{
+        changePlayer();
+    }
 }
-function restartGame() {
 
+function restartGame() {
+currentPlayer = "X"; 
+options = ["", "", "", "", "", "", "", "", ""];
+turnText.textContent = `${currentPlayer}'s turn`;
+cells.forEach(cell => cell.textContent = "");
+running = true; 
 }
